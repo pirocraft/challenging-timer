0b4aafeeaa4b71d33d86f5ee318afeb94f281ec0
GitHub Action <action@github.com>
2020-09-17 14:56:16 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/pirocraft/challengingtimer/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.pirocraft.challengingtimer.services
+
+import com.github.pirocraft.challengingtimer.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
