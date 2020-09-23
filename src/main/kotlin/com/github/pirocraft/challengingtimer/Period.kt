242418128bd6f8ad54434a8981b8aac48700828d
Pierre Rossato <pierre.rossato@gmail.com>
2020-09-23 23:30:09 +0200

    detekt check

--- src/main/kotlin/com/github/pirocraft/challengingtimer/Period.kt
+++ src/main/kotlin/com/github/pirocraft/challengingtimer/Period.kt
@@ -1,18 +1,21 @@
 package com.github.pirocraft.challengingtimer
 
+private const val SECONDS_IN_MINUTE = 60
+private const val LAST_SECOND_IN_A_MINUTE = 5
+
 data class Period(private val minutes: Int, private val seconds: Int) {
 
     /**
      * Total of seconds in this Period
      */
     fun inSeconds(): Int {
-        return seconds + minutes * 60
+        return seconds + minutes * SECONDS_IN_MINUTE
     }
 
     fun decrement(): Period {
         return when {
             seconds > 0 -> Period(minutes, seconds - 1)
-            seconds == 0 && minutes > 0 -> Period(minutes - 1, 59)
+            seconds == 0 && minutes > 0 -> Period(minutes - 1, LAST_SECOND_IN_A_MINUTE)
             else -> Period(minutes, seconds)
         }
     }
