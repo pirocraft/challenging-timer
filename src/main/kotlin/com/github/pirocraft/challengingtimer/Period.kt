93f507f2ebcfc9bdb6e3bc3e2980165a59faf6b6
Pierre Rossato <pierre.rossato@gmail.com>
2020-09-23 21:04:38 +0200

    Add Period decrement

--- src/main/kotlin/com/github/pirocraft/challengingtimer/Period.kt
+++ src/main/kotlin/com/github/pirocraft/challengingtimer/Period.kt
@@ -4,4 +4,12 @@ data class Period(val minutes: Int, val seconds: Int) {
     fun inSeconds(): Int {
         return seconds + minutes * 60
     }
+
+    fun decrement(): Period {
+        return when {
+            seconds > 0 -> Period(minutes, seconds - 1)
+            seconds == 0 && minutes > 0 -> Period(minutes - 1, 59)
+            else -> Period(minutes, seconds)
+        }
+    }
 }
--- src/test/kotlin/com/github/pirocraft/challengingtimer/PeriodTest.kt
+++ src/test/kotlin/com/github/pirocraft/challengingtimer/PeriodTest.kt
@@ -14,4 +14,16 @@ internal class PeriodTest {
         assertEquals(120 + 59, Period(2, 59).inSeconds())
         assertEquals(120 + 61, Period(2, 61).inSeconds())
     }
+
+    @Test
+    fun decrement() {
+        assertEquals(Period(0,0), Period(0, 1).decrement())
+        assertEquals(Period(0,1), Period(0, 2).decrement())
+        assertEquals(Period(0,2), Period(0, 3).decrement())
+        assertEquals(Period(0,0), Period(0, 0).decrement())
+        assertEquals(Period(1,0), Period(1, 1).decrement())
+        assertEquals(Period(0,59), Period(1, 0).decrement())
+        assertEquals(Period(2,1), Period(2, 2).decrement())
+        assertEquals(Period(1,59), Period(2, 0).decrement())
+    }
 }
