038897b8a6d9166dacf5b44907b7223fd90b425b
Pierre Rossato <pierre.rossato@gmail.com>
2020-10-29 20:17:32 +0100

    Indent

--- src/test/kotlin/com/github/pirocraft/challengingtimer/feature/RunCucumberTests.kt
+++ src/test/kotlin/com/github/pirocraft/challengingtimer/feature/RunCucumberTests.kt
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith
 
 @RunWith(Cucumber::class)
 @CucumberOptions(
-        features = ["src/test/resources/feature"],
-        tags = "not @ignored"
+    features = ["src/test/resources/feature"],
+    tags = "not @ignored"
 )
 class RunCucumberTests
