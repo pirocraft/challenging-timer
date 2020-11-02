6bdeecb25275a7d31b71ba0fb09dbaed4d083b01
Pierre Rossato <pierre.rossato@gmail.com>
2020-11-02 15:42:27 +0100

    Bind input changes to config view update

--- src/main/kotlin/com/github/pirocraft/challengingtimer/ui/ApplicationSettingsComponent.kt
+++ src/main/kotlin/com/github/pirocraft/challengingtimer/ui/ApplicationSettingsComponent.kt
@@ -6,6 +6,8 @@ import com.intellij.ui.components.JBTextField
 import com.intellij.util.ui.FormBuilder
 import javax.swing.JComponent
 import javax.swing.JPanel
+import javax.swing.event.DocumentEvent
+import javax.swing.event.DocumentListener
 
 class ApplicationSettingsComponent(private val configurationView: ConfigurationView) {
     fun content(): JPanel = FormBuilder.createFormBuilder()
@@ -16,8 +18,18 @@ class ApplicationSettingsComponent(private val configurationView: ConfigurationV
             .panel
 
     private fun inputField(): JComponent = JBTextField(configurationView.duration).apply {
-        addPropertyChangeListener {
-            configurationView.duration = it.newValue.toString()
-        }
+        document.addDocumentListener(object : DocumentListener {
+            override fun insertUpdate(e: DocumentEvent?) {
+                configurationView.duration = this@apply.text
+            }
+
+            override fun removeUpdate(e: DocumentEvent?) {
+                configurationView.duration = this@apply.text
+            }
+
+            override fun changedUpdate(e: DocumentEvent?) {
+                configurationView.duration = this@apply.text
+            }
+        })
     }
 }
